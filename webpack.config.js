c99bf1dd9850bc66820b44da827a1b1bb90674f8
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-17 21:09:28 +0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.noDeprecation = true;
+
+module.exports = {
+    entry: './source/index.js',
+    output: {
+        path: __dirname + '/dist/assets',
+        filename: 'bundle.js',
+        publicPath: 'assets',
+        sourceMapFilename: 'bundle.map'
+    },
+    devtool: '#source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            }
+        ]
+    },
+    plugin: [
+        new ExtractTextPlugin('bundle.css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            warnings: false,
+            mangle: true
+        })
+    ]
+};
