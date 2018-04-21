7ffa7639453560ebc9dd1f62ea3f03da2717e428
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-22 00:20:50 +0400

    changed MainMenu Component

--- source/components/ui/MainMenu.js
+++ source/components/ui/MainMenu.js
@@ -6,8 +6,11 @@ import '../../style/main-menu.css';
 const MainMenu = ({ className, user, onLogOut, location }) => 
     <nav className={(className ? `${className} main-menu` : 'main-menu')}>
         <ul className="main-menu__list">
+            {(location.pathname === '/profile' && user) ?
+                <li className="main-menu__item  main-menu__item--light">{`${user.firstName} ${user.middleName} ${user.lastName}`}</li> :
+                ''}
             <li className="main-menu__item">{(location.pathname === '/profile' && user) ?
-                <button type="button" onClick={onLogOut}>Выйти</button> :
+                <button className="main-menu__button" type="button" onClick={onLogOut}>Выйти</button> :
                 <NavLink to="/profile">Личный кабинет</NavLink>}</li>
         </ul>
     </nav>
