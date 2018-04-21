c966e60b2e88ecc718fd1411a9764e6b9e08b7ba
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-21 14:08:44 +0400

    changed MainMenu Component

--- source/components/ui/MainMenu.js
+++ source/components/ui/MainMenu.js
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-const MainMenu = () => 
-    <nav>
-        <ul>
-            <li><NavLink to="/profile">Личный кабинет</NavLink></li>
+import '../../style/main-menu.css';
+
+const MainMenu = ({ className }) => 
+    <nav className={(className ? `${className} main-menu` : 'main-menu')}>
+        <ul className="main-menu__list">
+            <li className="main-menu__item"><NavLink to="/profile">Личный кабинет</NavLink></li>
         </ul>
     </nav>
 ;
+
+MainMenu.propTypes = {
+    className: PropTypes.string,
+};
 
 export default MainMenu;
