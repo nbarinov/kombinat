3501b3f077476be2a9ee5a46ca21cd85aa94d69e
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-22 13:31:42 +0400

    renamed main-menu to menu

--- source/components/ui/MainMenu.js
+++ source/components/ui/MainMenu.js
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-import '../../style/main-menu.css';
+import '../../style/menu.css';
 
 const MainMenu = ({ className, user, onLogOut, location }) => 
-    <nav className={(className ? `${className} main-menu` : 'main-menu')}>
-        <ul className="main-menu__list">
+    <nav className={(className ? `${className} menu` : 'menu')}>
+        <ul className="menu__list">
             {(location.pathname === '/profile' && user) ?
-                <li className="main-menu__item  main-menu__item--light">{`${user.firstName} ${user.middleName} ${user.lastName}`}</li> :
+                <li className="menu__item  menu__item--light">{`${user.firstName} ${user.middleName} ${user.lastName}`}</li> :
                 ''}
-            <li className="main-menu__item">{(location.pathname === '/profile' && user) ?
-                <button className="main-menu__button" type="button" onClick={onLogOut}>Выйти</button> :
+            <li className="menu__item">{(location.pathname === '/profile' && user) ?
+                <button className="menu__button" type="button" onClick={onLogOut}>Выйти</button> :
                 <NavLink to="/profile">Личный кабинет</NavLink>}</li>
         </ul>
     </nav>
