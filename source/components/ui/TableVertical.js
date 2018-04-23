fb2daca24a2935f5f836fe88db0dc7035f84e276
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-23 21:03:46 +0400

    added TableVertical Component

---
+++ source/components/ui/TableVertical.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { t } from '../../config';
+import { dateFormatDote } from '../../libs/date-helper';
+
+import '../../style/ui/table.css';
+
+const TableVerical = ({ className, data }) => {
+    const headers = Object.keys(data || {});
+
+    return (
+        <table className={(className) ? `${className} table` : 'table'}>
+            {headers.map((head, row) =>
+                <tr key={row} className="table__tr">
+                    <th className="table__th table__th--vertical">{t[head]}</th>
+                    <td className={`table__td table__td--vertical table__td--${head}`}>
+                        {(head.includes('date')) ? dateFormatDote(data[head]) : data[head]}
+                    </td>
+                </tr>
+            )}
+        </table>
+    );
+};
+
+TableVerical.propTypes = {
+    className: PropTypes.string,
+    data: PropTypes.array.isRequired,
+};
+
+TableVerical.defaultProps = {
+    className: '',
+};
+
+export default TableVerical;
