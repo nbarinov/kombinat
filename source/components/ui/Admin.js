055c2df3a5ebfc19a81bcb8dabd66a7b1de22095
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-24 21:25:39 +0400

    fixed bugs

--- source/components/ui/Admin.js
+++ source/components/ui/Admin.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
 import AdminLogInForm from './AdminLogInFrom';
 import AdminMenu from './AdminMenu';
 import AdminPersons from './AdminPersons';
@@ -16,16 +17,18 @@ const Admin = ({ className, admin, onLogIn }) => {
         <AdminLogInForm className="login-form--center" onLogIn={onLogIn} /> :
         <section className={(className) ? `${className} admin` : 'admin'}>
             <AdminMenu className="admin__menu" />
-            <Route exact path="/admin" render={() => <section>
-                <h2>Здравствуйте, {admin.fname || admin.login}!</h2>
-            </section>} />
-            <Route path="/admin/persons/list" component={AdminPersons} />
-            <Route path="/admin/persons/view/:account" render={routeProps => <AdminPersonView {...routeProps} />} />
-            <Route path="/admin/schools/list" component={() => <AdminContainer url="/api/schools/list" search={true} />} />
-            <Route path="/admin/menus/list" component={AdminMenus} />
-            <Route path="/admin/menus/view/:id" render={routeProps => <AdminMenusView {...routeProps} />} />
-            <Route path="/admin/dishes/list" component={() => <AdminContainer url="/api/dishes/list" search={true} />} />
-            <Route component={Whoops404} />
+            <Switch>
+                <Route exact path="/admin" render={() => <section>
+                    <h2>Здравствуйте, {admin.fname || admin.login}!</h2>
+                </section>} />
+                <Route path="/admin/persons/list" component={AdminPersons} />
+                <Route path="/admin/persons/view/:account" render={routeProps => <AdminPersonView {...routeProps} />} />
+                <Route path="/admin/schools/list" component={() => <AdminContainer url="/api/schools/list" search={true} />} />
+                <Route path="/admin/menus/list" component={AdminMenus} />
+                <Route path="/admin/menus/view/:id" render={routeProps => <AdminMenusView {...routeProps} />} />
+                <Route path="/admin/dishes/list" component={() => <AdminContainer url="/api/dishes/list" search={true} />} />
+                <Route component={Whoops404} />
+            </Switch>
         </section>;
 };
 
