36c942d6356f7e43aa23496255f93c635aa0f4bc
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-05-11 01:57:34 +0400

    changed root page

--- source/components/App.js
+++ source/components/App.js
@@ -1,21 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
 
 import { Profile, Admin } from './containers';
-import DataComponent from './HOC/DataComponent';
-import List from './ui/List';
 import Header from './ui/Header';
 import Menus from './ui/Menus';
 import Whoops404 from './ui/Whoops404';
 
 import '../style/app.css';
 
-const PersonsList = DataComponent(List, '/api/persons');
-
 const App = () => (
     <div className="app">
         <Header className="app__header" />
         <Switch>
-            <Route exact path="/" component={PersonsList} />
+            <Route exact path="/" render={() => <p>
+                Система ведения базы данных, предназначена для систематизации учета<br />процесса горячего питания в общеобразовательных учреждениях.
+            </p>} />
             <Route path="/admin" component={() => <Admin className="admin--w100" />} />
             <Route path="/profile" component={Profile} />
             <Route path="/menus/:tin/:date" render={routeProps => <Menus {...routeProps} />} />
